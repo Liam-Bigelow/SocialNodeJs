097f85bc6d212efb53de13d7f4b08e6159dc949f
Liam-Bigelow <liamdbigelow@gmail.com>
2022-12-15 15:18:47 -0600

    Feat: user route layout developed

--- routes/user.route.js
+++ routes/user.route.js
@@ -9,11 +9,31 @@
 
 
 const express = require("express");
+const router = express.Router();
+
+const {ensureAuth} = require("./middleware/authentication.middleware" );
+
 const controller = require( "../controllers/user.controller");
 
 
+router.get( "/dashboard", ensureAuth, (req, res) => {
+    res.status(200).send( "User logged in..." );
+});
+
 // login
+router.post( "/login", (req, res) => {
+
+});
 
 // register
-
+router.post( "/register", (req, res) => {
+    // should take username and password as body parameters
+    controller.register( req.body.username, req.body.password )
+    .then( (newUser) => {
+        res.status( 200 ).json( newUser );
+    })
+    .catch( (error) => {
+        res.status( 500 ).json( error );
+    })
+});
 
