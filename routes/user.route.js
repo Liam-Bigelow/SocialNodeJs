f29a9350f906f5cec7451b1dd1f78a71f9941f17
Liam-Bigelow <liamdbigelow@gmail.com>
2022-12-15 15:08:31 -0600

    chore: need to make a login and register route

---
+++ routes/user.route.js
@@ -0,0 +1,19 @@
+
+
+/**
+ * -- Task ---
+ * 
+ * - user registration using unique username and password
+ * - user login
+ */
+
+
+const express = require("express");
+const controller = require( "../controllers/user.controller");
+
+
+// login
+
+// register
+
+
