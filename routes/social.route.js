67f80491c8997826ae62cd34d257a63b8e951bc5
Liam-Bigelow <liamdbigelow@gmail.com>
2022-12-16 14:27:32 -0600

    chore: changed order to match create

--- routes/social.route.js
+++ routes/social.route.js
@@ -14,12 +14,36 @@ const controller = require( "../controllers/social.controller");
 
 
 router.post( "/tweet", ensureAuth, (req, res) => {
+    controller.createTweet( req.user._id, req.user.username, req.body.message )
+    .then( (newTweet) => {
+        res.status( 200 ).json( newTweet );
+    })
+    .catch( (error) => {
+        console.error( error );
+        res.status( error.status ? error.status: 500 ).send( error );
+    });
 });
 
 router.get( "/tweet/:tweetId", ensureAuth, (req, res) => {
+    controller.getTweet( req.params.tweetId )
+    .then( (tweet) => {
+        res.status( 200 ).json( tweet );
+    })
+    .catch( (error) => {
+        console.error( error );
+        res.status( error.status ? error.status: 500 ).send( error );
+    });
 });
 
 router.put( "/tweet/:tweetId", ensureAuth, (req, res) => {
+    controller.updateTweet( req.user._id, req.params.tweetId, req.body.message )
+    .then( (updatedTweet) => {
+        res.status( 200 ).json( updatedTweet );
+    })
+    .catch( (error) => {
+        console.error( error );
+        res.status( error.status ? error.status: 500 ).send( error );
+    });
 });
 
 router.delete( "/tweet/:tweetId", ensureAuth, (req, res) => {
