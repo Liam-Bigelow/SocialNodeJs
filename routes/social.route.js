e05cc84aaf4d37a9e612b755bd200bd5c1633803
Liam-Bigelow <liamdbigelow@gmail.com>
2022-12-16 13:49:03 -0600

    chore: laying out route design for tweet operations

--- routes/social.route.js
+++ routes/social.route.js
@@ -2,4 +2,29 @@
 const express = require("express");
 const router = express.Router();
 
+const {ensureAuth} = require("./middleware/authentication.middleware" ); // assuming all tweets operations need to be authenticated first
+
+const controller = require( "../controllers/social.controller");
+
+/**
+ * -- Task ---
+ * 
+ * - Create, read, update, delete tweet
+*/
+
+
+router.post( "/tweet", ensureAuth, (req, res) => {
+});
+
+router.get( "/tweet/:tweetId", ensureAuth, (req, res) => {
+});
+
+router.put( "/tweet/:tweetId", ensureAuth, (req, res) => {
+});
+
+router.delete( "/tweet/:tweetId", ensureAuth, (req, res) => {
+});
+
+
+
 module.exports = router;
